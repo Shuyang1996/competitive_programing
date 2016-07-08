e4c4a7bda958ac02294fbdbc3ae27b8dbf39a159
Shuyang Yu <yu2@cooper.edu>
2016-07-07 22:55:12 -0400

    update removeElement

--- removeElement/removeElement.cpp
+++ removeElement/removeElement.cpp
@@ -2,43 +2,44 @@
 #include <vector>
 using namespace std;
 
-int removeElement (vector <int> &nums, int item) {
-	int index=0;
+int removeElement(vector<int> &nums, int item) {
+	int index = 0; // set up another pointer
 	int size = nums.size();
-	for( int i=0; i < size; ++i) {
+	for(int i=0; i<size; i++) {
 		if(nums[i]!= item) {
 			nums[index++] = nums[i];
 		}
 	}
 	return index;
-};
-	
-int main(){
+}
+
+int main () {
 	vector <int> nums;
 	nums.push_back(0);
 	nums.push_back(1);
+	nums.push_back(0);
 	nums.push_back(2);
+	nums.push_back(0);
 	nums.push_back(3);
-	
-	int previousLength = nums.size();
+	nums.push_back(0);
+	nums.push_back(4);
+	nums.push_back(0);
 
-	for(int j = 0; j < previousLength; j++){
-		
-		cout << nums[j] << endl;
-	}
-	
-	int length = removeElement(nums, 0);
+	int previousSize = nums.size();
 
-	cout << "Previous length is " << previousLength << endl;
+	cout << "Previous vector size: " << previousSize << endl;
+	cout << "Previous sample data set:" <<endl;
 
-	cout << "New length is " << length << endl;
-
-	for(int i = 0; i < length; i++){
-		cout << nums[i] << endl;
+	for(int i = 0; i < previousSize; i++) {
+		cout << nums[i] <<endl;
 	}
 
-	for(int i = 0; i < nums.size(); i++){
-		cout << nums[i] << endl;
+	int size = removeElement(nums,0);
+
+	cout <<"New vector size: " <<size << endl;
+	cout <<"New sample data set after removing target element" << endl;
+	for(int j=0; j < size; j++ ) {
+		cout <<nums[j] << endl;
 	}
 
 	return 0;
