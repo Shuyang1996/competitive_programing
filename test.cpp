0737d58ba882af690d10060bc9b293ca0be5b3f8
Shuyang Yu <yu2@cooper.edu>
2016-07-09 16:06:44 -0400

    trapwater

--- test.cpp
+++ test.cpp
@@ -3,38 +3,9 @@
 using namespace std;
 
 int main () {
-	int i=0;
-	int j=0;
-	cout << i++ << endl;
-	cout << i << endl;
-	cout << ++j << endl;
-
-	// vector<int> nums;
-
-	// nums.push_back(-5);	
-	// nums.push_back(6);	
-	// nums.push_back(-3);	
-	// nums.push_back(-4);	
-	// nums.push_back(0);	
-	// nums.push_back(3);	
-	// nums.push_back(2);	
-	// nums.push_back(1);	
-	// nums.push_back(0);	
-	// nums.push_back(4);
-	// nums.push_back(5);
-	// nums.push_back(-6);
-	// nums.push_back(3);
-	// nums.push_back(5);
-
-	// sort(nums.begin(),nums.end());
-	// int size =  nums.size();
-
-	// auto last = nums.end(); // last is a pointer 
-	// cout<< *(last-2) <<endl;
 	
-	// // for(int i =0; i < size; i++ ) {
-	// // 	cout<<nums[i]<<endl;
-	// // } 
+	int * left  =  new int;
+	int * right =  new int;
 
 	return 0;
 }
--- trapWater/trapWater.cpp
+++ trapWater/trapWater.cpp
@@ -1,14 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
 int trap(const vector<int>& A) {
 	const int n = A.size();
 
-	int *max_left = new int[n](); //
-	int *max_right = new int[n](); //
-
+	int *max_left = new int[n](); // there is a constructor, initializes everything to 0
+	int *max_right = new int[n](); // same as above. 
+	
 	for (int i = 1; i < n; i++) {
-	  max_left[i] = max(max_left[i - 1], A[i - 1]);
-	  max_right[n - 1 - i] = max(max_right[n - i], A[n - i]);
+		max_left[i] = max(max_left[i - 1], A[i - 1]);
+		max_right[n - 1 - i] = max(max_right[n - i], A[n - i]);
 	}
-
+	
 	int sum = 0;
 	for (int i = 0; i < n; i++) {
 		int height = min(max_left[i], max_right[i]);
@@ -20,4 +26,26 @@ int trap(const vector<int>& A) {
 	delete[] max_left;
 	delete[] max_right;
 	return sum;
+};
+
+int main () {
+	vector <int> nums;
+	
+	nums.push_back(0);
+	nums.push_back(1);
+	nums.push_back(0);
+	nums.push_back(2);
+	nums.push_back(1);
+	nums.push_back(0);
+	nums.push_back(1);
+	nums.push_back(3);
+	nums.push_back(2);
+	nums.push_back(1);
+	nums.push_back(2);
+	nums.push_back(1);
+
+	int sum = trap(nums);
+	cout << sum << endl;
+
+	return 0;
 }
