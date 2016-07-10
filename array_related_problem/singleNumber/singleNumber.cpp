ecf08ef56354c23eab95343bffbe822f66d1df28
Shuyang Yu <yu2@cooper.edu>
2016-07-10 15:08:40 -0400

    folder

---
+++ array_related_problem/singleNumber/singleNumber.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+int main () {
+	
+	vector <int> nums;
+	nums.push_back(0);
+	nums.push_back(1);
+	nums.push_back(0);
+	nums.push_back(2);
+	nums.push_back(2);
+	nums.push_back(1);
+	nums.push_back(3);
+	nums.push_back(6);
+	nums.push_back(4);
+	nums.push_back(3);
+	nums.push_back(5);
+	nums.push_back(5);
+	nums.push_back(4);
+
+
+
+	return 0;
+}
