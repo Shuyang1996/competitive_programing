3ae6183b0071bdca48746d65b6aaf33db299539f
Shuyang Yu <yu2@cooper.edu>
2016-07-14 01:35:51 -0400

    single list

---
+++ single_list_related_problem/addTwoNumber/addTwoNumber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+struct listNode {
+	int val;
+	ListNode * next;
+	ListNode (int x) : val(x), next(nullptr) {}; 
+}; // C++ member initializer list
+
+int main () {
+	return 0;
+}
