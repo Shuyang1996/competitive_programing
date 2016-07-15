d4a84a741476e737b6ba8f7a701f934308b864b1
Shuyang Yu <yu2@cooper.edu>
2016-07-15 17:45:17 -0400

    update addTwoNumbers

--- single_list_related_problem/addTwoNumber/addTwoNumber.cpp
+++ single_list_related_problem/addTwoNumber/addTwoNumber.cpp
@@ -3,84 +3,58 @@
 using namespace std;
 
 class Node {
-	public: 
+	public:
 		int val;
 		Node * next;
-		Node(int x) : val(x), next(NULL) {};
-
-		void setNext(Node * nextNode) {
-			next = nextNode;
+		Node(int x): val(x), next(NULL) {};
+		
+		void setNext (Node * nexeNode) {
+			next = nexeNode;
 		}
 };
 
+Node * addTwoNumbers ( Node * list1, Node * list2 ) {
+	//create start node 
+	Node initial (0);
+	Node * ptr = &initial;
+	int carry = 0;
 
-Node * addTwoNumbers ( Node * l1, Node *l2) {
-	Node dummy (-1);
-	int carry  = 0;
-	Node * prev = &dummy;
-	for (Node *pa = l1, *pb=l2; pa!=nullptr || pb!=nullptr; pa= pa==nullptr ? nullptr : pa->next, pb=pb==nullptr ?  nullptr :pb->next, prev = prev->next ) {
-		const int ai = pa == nullptr ? 0 :pa->val;
-		const int bi = pb == nullptr ? 0 :pb->val;
-		const int value = (ai + bi + carry) %10;
-		carry = (ai+bi+carry)/10;
-		prev->next = new Node (value);
+	// ptr1 and ptr2 are iterators for these two different lists; 
+	for( Node * ptr1 = list1, * ptr2 = list2; ptr1 != nullptr || ptr2 != nullptr; ptr1 = ptr1 == nullptr ? nullptr :ptr1->next, ptr2 = ptr2 == nullptr ? nullptr : ptr2->next, ptr = ptr->next) {
+		const int number1 = ptr1 == nullptr ? 0 : ptr1->val; // if ptr1 is out of bound, then it should be 0
+		const int number2 = ptr2 == nullptr ? 0 : ptr2->val;
+		const int value =  (number1+number2+carry)%10;
+		carry =  (number1+number2+carry)/10;
+		ptr->next = new Node(value);
 	}
-	if( carry > 0 ) {
-		prev->next = new Node(carry);
+	if(carry>0){
+		ptr->next = new Node(carry);
 	}
-
-	return dummy.next;
+	return initial.next;
 };
 
 
 int main () {
-	
-	Node * list1Number1 = new Node(2); // first node with value 2;
-	Node * list1Number2 = new Node(4); // second node with value 4;
-	Node * list1Number3 = new Node(3);
+	Node * list1number1 = new Node(2); // create a node pointer points at the first number 
+	Node * list1number2 = new Node(4); // another question why use new to allocate new memory
+	Node * list1number3 = new Node(3);
 
-	list1Number1->setNext(list1Number2);
-	list1Number2->setNext(list1Number3);
+	list1number1->setNext(list1number2);
+	list1number2->setNext(list1number3);
 
-	Node * list2Number1 = new Node(5); // first node with value 2;
-	Node * list2Number2 = new Node(6); // second node with value 4;
-	Node * list2Number3 = new Node(4);
+	Node * list2number1 = new Node(5); // create a node pointer points at the first number 
+	Node * list2number2 = new Node(6);
+	Node * list2number3 = new Node(4);
 
-	list2Number1->setNext(list2Number2);
-	list2Number2->setNext(list2Number3);
+	list2number1->setNext(list2number2);
+	list2number2->setNext(list2number3);
 
-	Node * finalResult = addTwoNumbers(list1Number1,list2Number1);
-
-	cout << finalResult->val << endl;
-
-	Node * secondResult = finalResult->next;
-	cout << secondResult->val << endl;
-
-	Node * thirdResult = secondResult->next;
-	cout << thirdResult->val << endl;
-
-	// list<int> list1;
-	// list<int> list2;
-	
-	// list1.push_back(2);
-	// list1.push_back(4);
-	// list1.push_back(3);
-
-	// list2.push_back(5);
-	// list2.push_back(6);
-	// list2.push_back(4);
-
-	// list <int>::iterator ptr1; // iterator 
-	// list <int>::iterator ptr2;
-
-	// for(ptr1 = list1.begin(); ptr1 != list1.end(); ptr1++) {
-	// 	cout << *ptr1 << endl;
-	// }
-
-	// for(ptr2 = list2.begin(); ptr2 != list2.end(); ptr2++) {
-	// 	cout << *ptr2 << endl;
-	// }
-	
+	Node * result = addTwoNumbers(list1number1, list2number1);
+	cout << result->val << endl;
+	Node * secondNumber =  result->next;
+	cout << secondNumber->val << endl;
+	Node * thirdNumber = secondNumber->next;
+	cout << thirdNumber->val << endl;
 
 	return 0;
 }
