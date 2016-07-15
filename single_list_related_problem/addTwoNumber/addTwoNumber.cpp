d8ef537dea126607306fce066f04d36f9dd1c500
Shuyang Yu <yu2@cooper.edu>
2016-07-14 20:25:16 -0400

    addTwoNumbers

--- single_list_related_problem/addTwoNumber/addTwoNumber.cpp
+++ single_list_related_problem/addTwoNumber/addTwoNumber.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
-struct listNode {
+struct ListNode {
 	int val;
 	ListNode * next;
-	ListNode (int x) : val(x), next(nullptr) {}; 
+	ListNode (int x) : val(x), next(NULL) {}; 
 }; // C++ member initializer list
 
 int main () {
+	list<int> list1;
+	list<int> list2;
+	
+	list1.push_back(2);
+	list1.push_back(4);
+	list1.push_back(3);
+
+	list2.push_back(5);
+	list2.push_back(6);
+	list2.push_back(4);
+
+	list <int>::iterator ptr1; // iterator 
+	list <int>::iterator ptr2;
+
+	for(ptr1 = list1.begin(); ptr1 != list1.end(); ptr1++) {
+		cout << *ptr1 << endl;
+	}
+
+	for(ptr2 = list2.begin(); ptr2 != list2.end(); ptr2++) {
+		cout << *ptr2 << endl;
+	}
+	
+
 	return 0;
 }
