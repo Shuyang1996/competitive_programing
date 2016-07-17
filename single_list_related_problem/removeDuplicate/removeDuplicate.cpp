ad8d6bfdf8a1d0a0ee00e5c588e7f86b63c9ea32
Shuyang Yu <yu2@cooper.edu>
2016-07-17 14:30:39 -0400

    finish remove duplicates

--- single_list_related_problem/removeDuplicate/removeDuplicate.cpp
+++ single_list_related_problem/removeDuplicate/removeDuplicate.cpp
@@ -19,7 +19,31 @@ class Node {
 		int getVal(){
 			return val;
 		}
+
+		void setVal(int x){
+			val = x;
+		}
 };
+
+Node * removeDuplicates (Node * list) {
+	//create initial node;
+	Node initial (0);
+	Node * ptr = &initial;
+	initial.setNext(list); // connect it with first node in sample data 
+	ptr =  ptr->getNext(); // pointing at the first element in the array 
+
+	for( Node * prev = ptr->getNext(); prev != nullptr; prev = prev->getNext()){
+		
+		const int val = prev->getVal();
+		if(ptr->getVal()!= val) {
+			ptr =  ptr->getNext();
+			ptr->setVal(val);
+		}
+	}
+
+	return initial.getNext();
+};
+
 
 int main () {
 	Node * number1 = new Node(1);
@@ -27,7 +51,7 @@ int main () {
 	Node * number3 = new Node(2);
 	Node * number4 = new Node(3);
 	Node * number5 = new Node(3);
-	Node * number6 = new Node(3);
+	Node * number6 = new Node(4);
 
 	number1->setNext(number2);
 	number2->setNext(number3);
@@ -51,5 +75,22 @@ int main () {
 
 	Node * number_6 =  number_5->getNext();
 	cout << number_6->getVal() << endl;
+
+	cout << "------------------------" << endl;
+
+	Node * newList = removeDuplicates(number1);
+
+	cout << newList->getVal() << endl;
+
+	Node * newList_2 = newList->getNext();
+	cout << newList_2->getVal() << endl;
+
+	Node * newList_3 =  newList_2->getNext();
+	cout << newList_3->getVal() << endl;
+
+	Node * newList_4 = newList_3->getNext();
+	cout << newList_4->getVal() << endl;
+
+
 	return 0;
 }
