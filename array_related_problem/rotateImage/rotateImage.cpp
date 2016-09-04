55e607f05526f8ba6fb83e4fea20885de846054c
Shuyang Yu <yu2@cooper.edu>
2016-09-04 17:13:53 -0400

    new rotate image

--- array_related_problem/rotateImage/rotateImage.cpp
+++ array_related_problem/rotateImage/rotateImage.cpp
@@ -2,94 +2,77 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int> > rotateImage (vector<vector<int> > &nums) {
-	const int size = nums[0].size(); 
-	
-	for(int i=0; i < size; i++) {
-		for(int j =0; j< size-i; j++) {
-			swap(nums[i][j],nums[size-1-j][size-1-i]);
+void rotateImage( vector<vector<int> > &image ) {
+
+	// flip the image by diagonal
+	int size =  image.size();
+	for(int i = 0; i < size-1; i++ ) {
+
+		for(int j = 0; j < size-1-i; j++ ) {
+			int value = image[i][j];
+			image[i][j] = image[size-1-j][size-1-i];
+			image[size-1-j][size-1-i] = value ;
 		}
 	}
 
-	//swap based on horizontally centered line
+	// then we flip the new image up side down
+	for(int i = 0; i < size/2; i++ ) {
 
-	for(int i = 0; i < size/2; i++) { //need to consider about even and odd number issue.
-		for( int j=0; j <size; j++) {
-			swap(nums[i][j],nums[size-1-i][j]);
+		for(int j = 0; j < size; j++ ) {
+			int value = image[i][j];
+			image[i][j] = image[size-1-i][j];
+			image[size-1-i][j] = value;
 		}
 	}
-
-	return nums;
 };
 
 int main () {
 
-	//create this two dimension vector using nested loop;
-	// vector <vector<int> > matrix;
-	// for(int i = 0; i < 2; i++){
-	// 	vector<int> row;
-	// 	for(int j= 0; j <2; j++) {
-	// 		row.push_back(j);
-	// 	}
-	// 	matrix.push_back(row);
-	// }
-	//
 
-	//create two dimension vector matrix manually
-	vector <vector<int> > newMatrix;
-	vector <int>row1;
-	vector <int>row2;
-	vector <int>row3;
-	vector <int>row4;
+	vector < vector <int> > image;
+	
+	vector <int> row1;
+	vector <int> row2;
+	vector <int> row3;
 
-	row1.push_back(3);
-	row1.push_back(2);
 	row1.push_back(1);
-	row1.push_back(4);
+	row1.push_back(2);
+	row1.push_back(3);
 
-	newMatrix.push_back(row1);
-
-	row2.push_back(1);
+	row2.push_back(4);
+	row2.push_back(5);
 	row2.push_back(6);
-	row2.push_back(7);
-	row2.push_back(3);
 
-	newMatrix.push_back(row2);
+	row3.push_back(7);
+	row3.push_back(8);
+	row3.push_back(9);
 
-	row3.push_back(0);
-	row3.push_back(2);
-	row3.push_back(1);
-	row3.push_back(5);
+
+	image.push_back(row1);
+	image.push_back(row2);
+	image.push_back(row3);
 	
-	newMatrix.push_back(row3);
+	cout << "Previous image is " << endl;
 
-	row4.push_back(4);
-	row4.push_back(2);
-	row4.push_back(7);
-	row4.push_back(9);
-	
-	newMatrix.push_back(row4);
-
-
-	for(int k = 0; k < newMatrix[0].size(); k++){
-		for(int h= 0; h <newMatrix[0].size(); h++) {
-			cout<<newMatrix[k][h];
+	for(int i = 0; i < 3; i++ ) {
+		for(int j = 0; j < 3; j++) {
+			cout << image[i][j];
 		}
-		cout<<'\n';
+		cout <<""<< endl;
 	}
 
-	cout <<'\n';
+	cout << "--------" << endl;
 
-	newMatrix = rotateImage(newMatrix);
-	
+	cout << "New image is " << endl;
 
-	for(int k = 0; k < newMatrix[0].size(); k++){
-		for(int h= 0; h <newMatrix[0].size(); h++) {
-			cout<<newMatrix[k][h];
+	rotateImage(image);
+
+	for(int i = 0; i < 3; i++ ) {
+		for(int j = 0; j < 3; j++) {
+			cout << image[i][j];
 		}
-		cout<<'\n';
+		cout <<""<< endl;
 	}
-
 
 	return 0;
 }
